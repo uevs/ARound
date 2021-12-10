11fe39bd15f44cf80e68553bdeac7737d236f813
uevs <36189306+uevs@users.noreply.github.com>
2021-12-10 16:00:20 +0100

    Initial Commit

---
+++ ARound/ARoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARoundApp.swift
+//  ARound
+//
+//  Created by leonardo on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ARoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
